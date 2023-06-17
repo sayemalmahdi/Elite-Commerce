8ceb29a0662a5672c90bc694eb982edd7f7e5c78
Sayem Mohammed Al-Mahdi <sayemalmahdi@gmail.com>
2023-06-17 16:37:07 +0600

    Added -- 'Deal of the Day' Design

--- resources/views/frontend/master.blade.php
+++ resources/views/frontend/master.blade.php
@@ -460,6 +460,9 @@
                                         <li><a href="elements-icon-boxes.html">Icon Boxes</a></li>
                                     </ul>
                                 </li>
+
+                                <li><a href="#">FAQs</a></li>
+
                             </ul><!-- End .menu -->
                         </nav><!-- End .main-nav -->
                     </div><!-- End .header-left -->
@@ -704,7 +707,65 @@
                 </div><!-- End .container -->
             </div><!-- End .bg-lighter -->
 
-            <div class="mb-6"></div><!-- End .mb-6 -->
+            <!-- <div class="mb-6"></div> -->
+            <!-- End .mb-6 -->
+            
+
+
+        <!-- Deal of the Day Start -->
+
+            <div class="bg-light deal-container pt-5 pb-3 mb-5">
+                <div class="container">
+                    <div class="row">
+
+                        <div class="col-lg-2">                        
+                        </div>
+
+                        <div class="col-lg-4">
+                            <div class="deal">
+                                <div class="deal-content">
+                                    <h4>Limited Quantities</h4>
+                                    <h2 style="color: #CC9965; font-size: 45px;">Deal of the Day</h2>
+
+                                    <h3 class="product-title"><a href="product.html">POÄNG</a></h3><!-- End .product-title -->
+                                    <br>                                     
+                                    <div class="product-price">
+                                        <span class="new-price">$149.00</span>
+                                        <span class="old-price">Was $240.00</span>
+                                    </div><!-- End .product-price -->
+
+                                    <div class="deal-countdown" data-until="+10h"></div><!-- End .deal-countdown -->
+                                    <br>
+
+                                    <a href="#" class="btn btn-primary">
+                                        <span>Shop Now</span><i class="icon-long-arrow-right"></i>
+                                    </a>
+
+                                </div><!-- End .deal-content -->
+                                <br/>                            
+
+                            </div><!-- End .deal -->
+                        </div><!-- End .col-lg-9 -->
+
+                        <div class="col-lg-4">
+                        <div class="deal-image">
+                                    <a href="#">
+                                        <!-- <img src="{{ asset('frontend')}}/assets/images/demos/demo-2/deal/product-1.jpg" alt="image"> -->
+                                        <!-- <img src="{{ asset('frontend')}}/assets/images/demos/demo-2/blog/post-1.jpg" alt="image"> -->
+                                        <img src="{{ asset('frontend')}}/assets/images/slider/slide-3-480w.jpg" alt="image">
+                                    </a>
+                                </div>
+                            </div><!-- End .banner -->
+                        
+
+                        <!-- End .col-lg-3 -->
+                    </div><!-- End .row -->
+                </div><!-- End .container -->
+            </div><!-- End .bg-light -->
+            
+        <!-- Deal of the Day End -->
+
+     
 
             <div class="container">
                 <div class="heading heading-center mb-3">
@@ -2311,7 +2372,7 @@
     <script src="{{ asset('frontend')}}/assets/js/jquery.waypoints.min.js"></script>
     <script src="{{ asset('frontend')}}/assets/js/superfish.min.js"></script>
     <script src="{{ asset('frontend')}}/assets/js/owl.carousel.min.js"></script>
-    <script src="{{ asset('frontend')}}/assets/js/jquery.magnific-popup.min.js"></script>
+    <script src="{{ asset('frontend')}}/assets/js/jquery.magnific-popup.min.js"></script>    
     <!-- Main JS File -->
     <script src="{{ asset('frontend')}}/assets/js/main.js"></script>
 </body>
